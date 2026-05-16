82411a9732fe8f9242e19d2df5099bf983a8e6d0
binibean <bin4981@gmail.com>
2026-05-17 06:43:51 +0900

    fix: add turbopack config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    turbopack: {},
+};
 
 export default withVanillaExtract(nextConfig);
